8f4c91c4988efb1e8a54635a300776aad4c99ea5
SiemonFrancart <siemon.francart@student.ehb.be>
2026-02-19 11:07:12 +0100

    Input field values assigned to variables

--- 01-interactive-bounce/main.js
+++ 01-interactive-bounce/main.js
@@ -3,14 +3,15 @@ let canvasElement = document.querySelector("#canvas");
 //1. Add variables needed for animating the ball
 let x = 300;
 let y = 300;
-let speedX = 2;
+let speedX = Math.random(0, 10);
 let directionX = 2;
-let speedY = 5;
+let directionY = 2;
+let speedY = Math.random(0, 10);
 
 //2. Add variables (size, speed, colorVal) for controlling the sketch
 let size = 30;
 let speed = 10;
-let colorVal = 5;
+let colorVal = "red";
 
 function setup() {
 	//Creating a canvas to draw on, using the existing canvas element.
@@ -22,19 +23,20 @@ function draw() {
 	background(220);
 
 	// 3.1 Animate position
-	x += speedX;
-	y += speedY;
+	x += speedX * directionX;
+	y += speedY * directionY;
 
 	// 3.2 Bounce logic: check the edges
 	if (x + size / 2 > 600 || x < size / 2) {
-		speedX *= -1;
+		directionX *= -1;
 	}
 
 	if (y + size / 2 > 600 || y < size / 2) {
-		speedY *= -1;
+		directionY *= -1;
 	}
 
 	// 3.3 Draw
+	fill(colorVal);
 	circle(x, y, size);
 }
 
@@ -47,5 +49,14 @@ let Cinput = document.querySelector("#color-input");
 
 // 5. Add Event Listeners; listen to the "input" event for real-time updates while dragging
 Sinput.addEventListener("input", function () {
-	size = Sinput.value;
+	size = Number(Sinput.value);
+});
+
+Spinput.addEventListener("input", function () {
+	speedX = Number(Spinput.value);
+	speedY = Number(Spinput.value) / 2;
+});
+
+Cinput.addEventListener("input", function () {
+	colorVal = Cinput.value;
 });
